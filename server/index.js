5c09a77060c66787820e4994bcbea6a4213e3eb3
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-11 01:54:59 +0530

    create a text api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import useRouter from "./routes/userRouter.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose
   });
 
 const app = express();
+
+app.use("/api/user", useRouter);
 
 app.listen(3000, () => {
   console.log("server listing on 3000");
