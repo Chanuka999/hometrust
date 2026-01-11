7448c8561812025f354334a3ec43b5129fbe9e7d
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-12 02:27:33 +0530

    refactor signup function to use next for error handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcryptjs, { hashSync } from "bcryptjs";
 
-export const singup = async (req, res) => {
+export const singup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
@@ -11,6 +11,6 @@ export const singup = async (req, res) => {
       message: "data added successfully",
     });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
