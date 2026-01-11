c491617c60a21b637f7a0ab9ef3c7982a80db967
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-12 01:57:47 +0530

    add authRouter to handle authentication routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import useRouter from "./routes/userRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 dotenv.config();
 
@@ -15,8 +16,10 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", useRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("server listing on 3000");
