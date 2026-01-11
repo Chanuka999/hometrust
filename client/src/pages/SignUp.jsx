6541b3103c01b52f52eca8b5c2b6056ed39e6df2
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-12 02:40:10 +0530

    implement SignUp form with username, email, and password fields

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>signUp</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
