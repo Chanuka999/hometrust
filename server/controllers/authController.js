0d98d9c991111d2cf9d043af79fa7e881714b8b5
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-12 21:23:08 +0530

    chnange name url signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcryptjs, { hashSync } from "bcryptjs";
 
-export const singup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
