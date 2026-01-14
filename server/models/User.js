d2828dad3d3bfe049a6a442805d6fdb81ab48c47
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-15 01:39:13 +0530

    insert avatar from user.js page

--- server/models/User.js
+++ server/models/User.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNKfj6RsyRZqO4nnWkPFrYMmgrzDmyG31pFQ&s",
+    },
   },
   { timestamps: true }
 );
